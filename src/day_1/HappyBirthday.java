febffbb6531cd4f7aa6fc016db47307f67c6d08b
Parizat0129 <parizat0129@gmail.com>
2020-04-28 15:14:29 -0500

    HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,7 +3,8 @@ package day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
        
-        System.out.println("Happy birthday B18 again, this line is from GigHub")
+        System.out.println("Happy birthday B18 again, this line is from GigHub");
+        System.out.println("Happy ");
 
     }
 }
