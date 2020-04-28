c7022df3ac8f44119531d6ea3773a79f2b29ac3b
Parizat0129 <64176102+Parizat0129@users.noreply.github.com>
2020-04-28 14:51:20 -0500

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -2,6 +2,8 @@ package day_1;
 
 public class HappyBirthday {
     public static void main(String[] args) {
+       
+        System.out.println("Happy birthday B18 again, this line is from GigHub")
 
     }
 }
